f8d64813d6e30ab97818efcf9bbab6fb7e5d43fe
Eze-Din <ezedinnigussie2@gmail.com>
2022-09-13 00:03:20 -0700

    has been commited

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/** * main - Entry point * Description: prints '_putchar \n' * Return: Always 0 (success) */
+int main(void){
+char str[] = "_putchar\n";
+int i = 0; 
+
+while (str[i] != '\0')
+{	
+char c = str[i];
+	
+_putchar(c);
+	i++;
+}
+return (0);
+}
