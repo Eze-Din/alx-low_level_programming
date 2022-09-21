7f5e35dc6f8b183eb0de500fc4a3c1c10a9ee394
Eze-Din <ezedinnigussie2@gmail.com>
2022-09-20 23:09:36 -0700

    has been commited

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: string pointer
+ *
+ * Return: string capitalized string.
+ */
+char *cap_string(char *str)
+{
+	int i = 0, j;
+	char s1, s2;
+	char sp[] = "\t\n,;.!\?\"(){} ";
+
+	while (str[i] != '\0')
+	{
+		s1 = *(str + i);
+		if (s1 >= 'a' && s1 <= 'z')
+		{
+			if (i == 0)
+				str[i] = s1 - 32;
+			else
+			{
+				s2 = str[i - 1];
+				for (j = 0; sp[j] != '\0'; ++j)
+				{
+					if (s2 == sp[j])
+						str[i] = s1 - 32;
+				}
+			}
+
+		}
+		i++;
+	}
+	return (str);
+}
