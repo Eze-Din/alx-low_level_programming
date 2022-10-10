9591e2bd7ee6442c7d2b70e3fdd4f04bad59ff82
Eze-Din <ezedinnigussie2@gmail.com>
2022-10-09 23:19:25 -0700

    has been commited

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* print_dog - print dog structure
+*
+* @d: dog structure object
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+
+		printf("Age: %0.6f\n", d->age);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
