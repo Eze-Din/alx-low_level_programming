d773069f66a1a801ccdac42c9f171aedba4a8ef6
Eze-Din <ezedinnigussie2@gmail.com>
2022-10-20 01:56:41 -0700

    has been commited

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * first - prints a sentence before the main function is executed.
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
